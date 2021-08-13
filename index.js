7a753872d06c166760338f468b95c9d142d7a267
Konteh <kontehebrima993@gmail.com>
2021-08-13 01:21:27 -0700

    render without input form

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent
 if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(
 function(position){
@@ -19,16 +20,28 @@ function(position){
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
         const coords = [latitude,longitude]
-        const map = L.map('map').setView(coords, 13);
+         map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
       
-        map.on('click', function(mapEvent){
-          
-            const{lat,lng} = mapEvent.latlng
+        map.on('click', function(mapE){
+        mapEvent = mapE;
+          form.classList.remove('hidden');
+          inputDistance.focus()
+            
+        })
+}, 
+function(){
+
+})
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    const{lat,lng} = mapEvent.latlng
 
             L.marker([lat,lng])
             .addTo(map)
@@ -42,8 +55,9 @@ function(position){
             )
             .setPopupContent('worked')
             .openPopup();
-        })
-}, 
-function(){
+})
 
+inputType.addEventListener('change', function(e){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
 })
